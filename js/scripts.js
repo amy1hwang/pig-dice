b32bffc3cffd9386eebf12033939ff233c88de9d
Amy Hwang and Ryan Peterson <amy@>
2016-11-02 10:12:22 -0700

    add pig game objects and methods

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,43 @@ function Player(name){
   this.name = name;
   this.currentScore = 0;
   this.totalScore = 0;
-}
+};
 
 function Game(player){
   this.pigGame = [];
-}
+  this.dieValue = 1;
+};
+
+Game.prototype.roll = function() {
+  return Math.floor(Math.random() * (7 - 1)) + 1;
+};
+
+Game.prototype.gamePlay = function() {
+  var x = 0;
+  while(this.dieValue > 1 || this.dieValue != "hold"){
+    //do stuff
+  }
+};
 
 var newGame = new Game();
 
 $(document).ready(function() {
   $("form#player").submit(function(event){
-    event.preventDefault();
     var name = $("#player-entry").val();
     var newPlayer = new Player(name);
 
     newGame.pigGame.push(newPlayer);
+    $(".show-player-name").append(name + "<br>");
+    event.preventDefault();
+  });
+
+  $("form#game").submit(function(event){
+
+    $("#hold").click(function(){
+      newGame.dieValue = "hold";
+    });
+    newGame.dieValue = newGame.roll();
+    $("#display h3").text(newGame.roll());
+    event.preventDefault();
   });
 })
